172386c756dd0d069a09576af9118b8d43d1a98e
BEATRIZ MONTEIRO RODRIGUES <25170170@educ123.sp.senai.br>
2025-11-11 09:08:02 -0300

    login rh

---
+++ frontend/src/app/LoginRH/page.jsx
@@ -0,0 +1,70 @@
+'use client';
+import React, { useState } from 'react';
+import './loginrh.css';
+
+export default function LoginRH() {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Email:', email);
+    console.log('Senha:', senha);
+  };
+
+  return (
+   
+   
+   
+   <div className="container">
+      <div className="login-box">
+        <div className="header">
+
+        <div className="logo">
+          <img src="../imagens/Logo.png" width={220} alt="Logo GM" />
+        </div>
+          <div className="icon"></div>
+          <h2>Acesso RH</h2>
+          <p>Portal Administrativo GM</p>
+        </div>
+
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="email">Email Corporativo</label>
+            <input
+              type="email"
+              id="email"
+              placeholder="seuemail@gm.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="senha">Senha</label>
+            <input
+              type="password"
+              id="senha"
+              placeholder="********"
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="remember-me">
+            <input type="checkbox" id="lembrar" />
+            <label htmlFor="lembrar">Lembrar-me</label>
+          </div>
+
+          <button type="submit" className="btn">
+            Entrar no Sistema
+          </button>
+        </form>
+
+        
+      </div>
+    </div>
+  );
+}
